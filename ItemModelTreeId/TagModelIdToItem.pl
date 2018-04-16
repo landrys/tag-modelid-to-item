57587342e156c8c221dd672e3c08c6ac25680103
Leon Landry <landryseleven@gmail.com>
2018-04-16 15:37:16 -0400

    Added mailing problems when updating the item model Id Foreign Key

--- ItemModelTreeId/TagModelIdToItem.pl
+++ ItemModelTreeId/TagModelIdToItem.pl
@@ -10,11 +10,12 @@ use MyModules::Bean::ItemTag;
 use MyModules::Eleven::ItemTagModelIdMySqlCaller;
 use MyModules::Eleven::ItemModelIdUpdater;
 use Data::Dumper;
+use MyModules::MyMailer;
 
 my $NOW =  strftime "%F %T", localtime $^T;
 
-my $filename = 'BadModelIds';
-open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+#my $filename = 'BadModelIds';
+#open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
 
 my $modelIdUpdater = getUpdater();
 $modelIdUpdater->connect();
@@ -23,40 +24,41 @@ my $itemTagSelector = getElevenCaller();
 $itemTagSelector->connect();
 $itemTagSelector->getItemTagsWithModelId();;
 
+my $errors = "\n";
 my $count = 0;
 foreach my $itemTagModelId (@{$itemTagSelector->itemTagsModelId}) {
+    my $modelId;
     eval {
         my @modelIdSplitUp = split(/:/,$itemTagModelId->tag);
-        my $modelId = $modelIdSplitUp[1];
+        $modelId = $modelIdSplitUp[1];
         my $result = $modelIdUpdater->updateItem($modelId, $itemTagModelId->item);
-        if ( $result->status == -1 ) {
-            print $fh $modelId . ',' . $itemTagModelId->item  . "\n";
-        } elsif ( $result->status == 0 ) {
-            say 'Some other error occured.';
-            say Dumper($result);
-        } else {
-            # Ok we are good
-            $count++;
-        }
+        $count++;
         1;
     } || do {
         my $e = $@;
+        $errors = $errors . "\n" . ' itemId:modelId ' . $itemTagModelId->item . ':' . $modelId . "\n" . $e . "\n";
     };
 }
 
 $itemTagSelector->disconnect();
 $modelIdUpdater->disconnect();
-close $fh;
 say "Updated " . $count . " items.";
+
+if (  length $errors > 5 ) {
+    my $mailer = getMailer();
+    $mailer->subject("Item Tag Model Id to Item Model Id FK errors");
+    $mailer->body($errors);
+    $mailer->sendMessage();
+}
 
 sub getElevenCaller {
 
     my $object = MyModules::Eleven::ItemTagModelIdMySqlCaller->new(
         archived => 0,
-        database => 'vvvvvv',
+        database => 'eleven',
         host  => 'localhost',
-        username   => 'zzzzzz',
-        password   => 'wwwwww',
+        username   => 'xxxxx',
+        password   => 'xxxxx',
     );
 
     return $object;
@@ -67,11 +69,27 @@ sub getUpdater {
 
     my $object = MyModules::Eleven::ItemModelIdUpdater->new(
         archived => 0,
-        database => 'vvvvvv',
+        database => 'eleven',
         host  => 'localhost',
-        username   => 'xxxxxx',
-        password   => 'yyyyyy',
+        username   => 'xxxx',
+        password   => 'xxxx',
     );
 
     return $object;
 }
+
+sub getMailer {
+
+    my $self = shift;
+
+    my $object = MyModules::MyMailer->new(
+        body => '',
+        recipient => 'fpiergen@landrys.com',
+        message => undef,
+
+    );
+
+    return $object;
+}
+
+
